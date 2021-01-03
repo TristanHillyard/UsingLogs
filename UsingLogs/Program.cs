f9e857615f18911ed684708fdad810f3873f25af
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2021-01-03 14:47:55 -0700

    Add project files.

---
+++ UsingLogs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace UsingLogs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            string usernum = Convert.ToString(Console.ReadLine());
+            File.WriteAllText(@"C:\Users\tristan.hillyard\Logs\logs.text", usernum);
+            Console.WriteLine(File.ReadAllText(@"C:\Users\tristan.hillyard\Logs\logs.text"));
+        }
+    }
+}
